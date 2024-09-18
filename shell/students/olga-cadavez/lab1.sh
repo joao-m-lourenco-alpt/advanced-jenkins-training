892eb9aa83b028553ff040656c9173c302aea32e
olga092024 <cadavezolga@gmail.com>
2024-09-18 10:49:12 +0000

    Lab1

--- shell/students/olga-cadavez/lab1.sh
+++ shell/students/olga-cadavez/lab1.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
-echo Hello World
+read -p "what is your first name?" firstname
+read -p "what is your last name?" lastname
+read -p "where were you born?"  place
+
+echo  "Nice to meet you   $firstname $lastname from $place"
